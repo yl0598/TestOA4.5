7e9059cbffc787fc38640d2376ecf4b3eda77548
yl0598 <664160465@qq.com>
2017-11-07 22:55:53 +0800

    分布式缓存memcache

---
+++ OA.Common/MemcacheHelper.cs
@@ -0,0 +1,75 @@
+﻿using Memcached.ClientLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Common
+{
+   public class MemcacheHelper
+    {
+        public static readonly MemcachedClient mc;
+      static MemcacheHelper()
+        {
+            string[] serverlist = {"127.0.0.1 12000" };
+            //初始化池
+            SockIOPool pool = SockIOPool.GetInstance();
+            pool.SetServers(serverlist);
+
+            pool.InitConnections = 3;
+            pool.MinConnections = 3;
+            pool.MaxConnections = 4;
+
+            pool.SocketConnectTimeout = 1000;
+            pool.SocketTimeout = 3000;
+
+            pool.MaintenanceSleep = 30;
+            pool.Failover = true;
+
+            pool.Nagle = false;
+            pool.Initialize();
+
+
+            //获取客户端实例
+           mc = new MemcachedClient();
+            mc.EnableCompression = false;
+        }
+        /// <summary>
+        /// 向memcache存储数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void Set(string key ,object value)
+        {
+            mc.Set(key, value);
+
+        }
+        public static void Set(string key,object value,DateTime time)
+        {
+            mc.Set(key, value, time);
+        }
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static object Get(string key)
+        {
+            return mc.Get(key);
+        }
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool Delete(string key)
+        {
+            if (mc.KeyExists(key))
+            {
+                return mc.Delete(key);
+            }
+            return false;
+        }
+    }
+}
