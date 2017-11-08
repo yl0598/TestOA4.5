03f0b8ed5f63e7ced4d8afda2f70edbed13746ed
yl0598 <664160465@qq.com>
2017-11-08 23:09:54 +0800

    验证码

---
+++ OA.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OA.WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+ 
+        public ActionResult ValidateCode()
+        {
+            Common.ValidateCode validateCode = new Common.ValidateCode();
+            string code= validateCode.CreateValidateCode(4);
+            byte[] buffer=validateCode.CreateValidateGraphic(code);
+            return File(buffer,"image/jpeg");
+        }
+    }
+}
